e07bc74d809b8deeb7bade490788cf51935bf35a
anonymousOlymp <anony44ou5@yandex.ru>
2025-02-09 21:33:36 +0300

    add tests

--- test/include/test.h
+++ test/include/test.h
@@ -2,6 +2,7 @@
 #define TEST_H
 
 #include <iostream>
+#include <string>
 
 #define assertEquals(a, b) \
     if ((a) != (b)) { \
@@ -20,5 +21,17 @@
         std::cerr << __FUNC__ << " Assert fail: " #condition " must be false" << std::endl; \
         return; \
     }
+
+#define assertThrowsWithMessage(expression, type, message) \
+    try { \
+        expression; \
+        std::cerr << __FUNC__ << " Assert fail: " #expression " not throws" << std::endl; \
+        return; \
+    } catch (type &exception) { \
+        assertEquals(std::string(exception.what()), std::string(message)); \
+    } catch (...) { \
+        std::cerr << __FUNC__ << " Assert fail: " #expression " not throws " #type << std::endl; \
+    }
+
 
 #endif // TEST_H
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,3 +1,9 @@
+#include "test_board.h"
+#include "test_checker.h"
+#include "test_game.h"
+#include "test_player.h"
+#include "test_position.h"
+
 
 int main(int argc, const char** argv) {
     return 0;
